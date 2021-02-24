ee5224cce65ab57bba5496610297ae5452208262
Henry Coleman <henrycoleman@henrys-mbp.lan>
2021-02-23 21:47:40 -0600

    script file

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const $title = $('#title');
 const $temp = $('#temp');
 const $index = $('#index');
 const $desc = $('#desc');
-const $cityName = $('input[type="text"]');
+const $input = $('input[type="text"]');
 
 const API_Key = '3ef3ac19f4ab74e5d4cc0765c07ba5a2';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
@@ -18,20 +18,19 @@ function handleGetData (event) {
     // calling preventDefault() on a 'submit' event will prevent a page refresh  
  userInput = $input.val();
    // getting the user input
-
+console.log(userInput)
     $.ajax(BASE_URL + userInput + '&appid=' + API_Key)
      .then(function(data) {
          weatherData = data;
          render();
      }, function(error) {
          console.log('Error:', error);
-     })
+     });
     
 }
-
-function render() {
-    $title.text(weatherData.title);
-    $temp.text(weatherData.temp);
-    $index.text(weatherData.main.);
+ function render() {
+    $title.text(weatherData.name);
+    $temp.text(weatherData.main.temp);
+    $index.text(weatherData.main.feels_like);
     $desc.text(weatherData.weather[0].description);
  }
